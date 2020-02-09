dc8b63d22c0b97ec06c8fa6e4c637f5ebe6031d0
Dave Simmons <shortlyportly@googlemail.com>
2020-02-09 08:56:38 +0000

    Added Bootstrap

--- src/ring_app/home.clj
+++ src/ring_app/home.clj
@@ -6,10 +6,12 @@
 (defn home-page []
   (html5
    [:html
-    [:head]
+    [:head
+     (include-css "/css/bootstrap.min.css")]
     [:body
      [:h1 "Welcome Home"]
-     (include-js "/wibble.js")]]))
+     [:button {:class "btn btn-primary"} "Hello"]
+     (include-js "/js/jquery-3.4.1.min.js" "/js/bootstrap.js")]]))
 
 (def routes
   ["/" {:get (fn [req] (content-type (ok (home-page)) "text/html; charset=utf-8"))}])
